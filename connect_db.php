8b0b2f07d5d246b0791404ae8466f3ab99c8b08a
gia_thuan <giathuan2388@gmail.com>
2021-08-28 17:28:57 +0700

    basic project

--- connect_db.php
+++ connect_db.php
@@ -1,6 +1,6 @@
 <?php
     // require_once('../config/config.php');
-    $conn = new mysqli("localhost","root","","ecommerce");
+    $conn = new mysqli("localhost","root","","market");
     mysqli_set_charset($conn, 'UTF8');
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
